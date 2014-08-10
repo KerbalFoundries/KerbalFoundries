8f49c0e985469038b2712af2f7dbe245a8d917a9
Lo-FiOrbitalScience <ian.elvar@gmail.com>
2014-08-10 14:30:27 +0100

    Added WIP ModuleLandingGear
    Whether we need another landing gear module is debatable, though I
    suppose another Open Source project won't hurt. Steering, retract and
    wheel rotation animation are not implemented yet as I've been playing
    with making swing-arm suspension work. The last parts are all trivial,
    however :)

--- SRC/LandingGearWheel.cs
+++ SRC/LandingGearWheel.cs
@@ -25,39 +25,45 @@ namespace KerbalFoundries
 
         public override void OnStart(PartModule.StartState state)
         {
+            foreach (WheelCollider wc in this.part.GetComponentsInChildren<WheelCollider>())
+            {
+                if (wc.name.Equals(colliderName, StringComparison.Ordinal))
+                {
+                    wheelCollider = wc;
+                }
+            }
+            foreach (Transform tr in this.part.GetComponentsInChildren<Transform>())
+            {
+                if (tr.name.Equals(susTravName, StringComparison.Ordinal))
+                {
+                    susTrav = tr;
+                }
+            }
+            foreach (Transform tr in this.part.GetComponentsInChildren<Transform>())
+            {
+                if (tr.name.Equals(wheelName, StringComparison.Ordinal))
+                {
+                    wheel = tr;
+                }
+            }
             print("LandingGearWheel Called");
             if (HighLogic.LoadedSceneIsEditor)
             {
 
+                initialTraverse = susTrav.transform.localPosition;
+                lastTempTraverse = initialTraverse.y - wheelCollider.suspensionDistance - 0.035f; //sets it to a default value for the sprockets
+                Vector3 editorTraverse = initialTraverse;
+                editorTraverse.y = lastTempTraverse;
+                susTrav.localPosition = editorTraverse;
             }
             if (HighLogic.LoadedSceneIsFlight)
             {
                 //find names onjects in part
                 this.part.force_activate();
-                foreach (WheelCollider wc in this.part.GetComponentsInChildren<WheelCollider>())
-                {
-                    if (wc.name.Equals(colliderName, StringComparison.Ordinal))
-                    {
-                        wheelCollider = wc;
-                    }
-                }
-                foreach (Transform tr in this.part.GetComponentsInChildren<Transform>())
-                {
-                    if (tr.name.Equals(susTravName, StringComparison.Ordinal))
-                    {
-                        susTrav = tr;
-                    }
-                }
-                foreach (Transform tr in this.part.GetComponentsInChildren<Transform>())
-                {
-                    if (tr.name.Equals(wheelName, StringComparison.Ordinal))
-                    {
-                        wheel = tr;
-                    }
-                }
+
 
                 initialTraverse = susTrav.transform.localPosition;
-                lastTempTraverse = initialTraverse.z; //sets it to a default value for the sprockets
+                lastTempTraverse = initialTraverse.y - wheelCollider.suspensionDistance - 0.035f; //sets it to a default value for the sprockets
             }
             //end find named objects
             base.OnStart(state);
@@ -65,19 +71,20 @@ namespace KerbalFoundries
 
         public override void OnUpdate() 
         {
-            base.OnUpdate();
+            base.OnUpdate(); 
             WheelHit hit;
             Vector3 tempTraverse = initialTraverse;
             bool grounded = wheelCollider.GetGroundHit(out hit); //set up to pass out wheelhit coordinates
             if (grounded) //is it on the ground
             {
-                tempTraverse.z += (-wheelCollider.transform.InverseTransformPoint(hit.point).y) + wheelCollider.radius;// / wheelCollider.suspensionDistance; //out hit does not take wheel radius into account
-                lastTempTraverse = tempTraverse.z;
+                tempTraverse.y -= (-wheelCollider.transform.InverseTransformPoint(hit.point).y) - wheelCollider.radius;// / wheelCollider.suspensionDistance; //out hit does not take wheel radius into account
+                lastTempTraverse = tempTraverse.y;
             }
             else
             {
-                tempTraverse.z = lastTempTraverse;
+                tempTraverse.y = lastTempTraverse;
             } //movement defaults back to zero when not grounded
+            print(tempTraverse.y);
             susTrav.transform.localPosition = tempTraverse; //move the suspensioTraverse object
         }
     }
